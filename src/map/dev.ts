32780dba96c7ea3ae1131265c1504df91c1bccce
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2023-07-26 17:09:35 +0200

    show inspector by default

--- src/map/dev.ts
+++ src/map/dev.ts
@@ -2,6 +2,8 @@ import { Scene } from "@babylonjs/core";
 
 export const initDebugLayer = (scene: Scene) => {
   if (import.meta.env.DEV) {
+    scene.debugLayer.show();
+
     // hide/show the Inspector
     window.addEventListener("keydown", (e) => {
       if (e.shiftKey && e.ctrlKey && e.key === "I") {
