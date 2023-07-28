d60ef3d7747d73c90e4d450946913b5fb19e17e5
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2023-07-28 03:38:05 +0200

    add new getGrid helper

---
+++ src/utils/grid.ts
@@ -0,0 +1,51 @@
+import { HexId } from "./hexId";
+
+export const SIDE_LENGTH_PER_TRIBE = 3;
+
+export type TGridTile = {
+  colIndex: number;
+  label: string;
+  rowIndex: number;
+};
+
+export const getGrid = (tribesCount: number) => {
+  const GRID_SIDE = tribesCount * SIDE_LENGTH_PER_TRIBE;
+  const rowsCount = GRID_SIDE * 2 - 1;
+  const sideLength = GRID_SIDE;
+  const middleRow = sideLength;
+
+  const array: TGridTile[][] = [];
+
+  for (
+    let rowIndex = 0, lastCol = sideLength;
+    rowIndex < rowsCount;
+    rowIndex++
+  ) {
+    for (let colIndex = 0; colIndex < lastCol; colIndex++) {
+      const label = HexId.fromArray([rowIndex, colIndex]);
+
+      const isGrowing = rowIndex < middleRow - 1;
+      const modifier = isGrowing ? 1 : -1;
+
+      lastCol += modifier;
+
+      const tile: TGridTile = {
+        colIndex,
+        label,
+        rowIndex,
+      };
+
+      if (!array[rowIndex]) {
+        array[rowIndex] = [tile];
+      } else {
+        array[rowIndex][colIndex] = tile;
+      }
+    }
+  }
+
+  return {
+    array,
+    rowsCount,
+    sideLength,
+  };
+};
