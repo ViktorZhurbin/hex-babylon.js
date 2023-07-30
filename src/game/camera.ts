a3af250e246820735fe0383ef84f26eb3e5923c8
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2023-07-30 16:01:25 +0200

    adjust camera settings

--- src/game/camera.ts
+++ src/game/camera.ts
@@ -12,16 +12,16 @@ export const initCamera = (scene: Scene, canvas: HTMLCanvasElement) => {
     scene,
   );
 
-  camera.lowerRadiusLimit = 5;
   camera.lowerAlphaLimit = 0;
-  // camera.upperAlphaLimit = 0;
-  camera.lowerBetaLimit = 0;
-  camera.upperBetaLimit = 1.5;
+  camera.upperAlphaLimit = 0;
+  camera.lowerBetaLimit = 0.5;
+  camera.upperBetaLimit = 1.2;
 
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
 
-  camera.radius = SIDE_LENGTH_PER_TRIBE * 6;
+  camera.radius = SIDE_LENGTH_PER_TRIBE * 9;
+  camera.lowerRadiusLimit = 6;
   camera.upperRadiusLimit = camera.radius + 6;
 
   return camera;
