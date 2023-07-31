0e7678cff3b5b0fce1359301f1a815d66bd64792
Viktor Zhurbin <vzhurbin@gmail.com>
2023-07-31 22:59:40 +0200

    add comment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // See FAQ at https://doc.babylonjs.com/setup/frameworkPackages/es6Support#side-effects
 import "@babylonjs/core/Culling/ray"; // for Ray and Picking functions
-import "@babylonjs/core/Rendering/edgesRenderer";
+import "@babylonjs/core/Rendering/edgesRenderer"; // for mesh.enableEdgesRendering()
 
 import { initGame } from "./game";
 import "./style.css";
